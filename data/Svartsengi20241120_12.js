69f736b8877dd5658d5154cc792f0b5e6bf715f0
KleinerGeologe <david.guenzler@yahoo.de>
2025-04-01 13:00:03 +0200

    Add files via upload

---
+++ data/Svartsengi20241120_12.js
@@ -0,0 +1,1 @@
+var json_Svartsengi20241120_12 = {"type":"FeatureCollection","name":"Svartsengi20241120_12","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[]}
