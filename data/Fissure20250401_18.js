b12b677ddf7ae296a6bd7c08659a08d0bdf657db
KleinerGeologe <david.guenzler@yahoo.de>
2025-04-06 19:49:41 +0200

    new data

--- data/Fissure20250401_18.js
+++ data/Fissure20250401_18.js
@@ -1,1 +1,12 @@
-var json_Fissure20250401_18 = {"type":"FeatureCollection","name":"Fissure20250401_18","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"id":"1"},"geometry":{"type":"MultiLineString","coordinates":[[[-22.407553614863037,63.861428801219418],[-22.412659102256942,63.858159171952309]]]}},{"type":"Feature","properties":{"id":"2"},"geometry":{"type":"MultiLineString","coordinates":[[[-22.418501886851384,63.854303230009705],[-22.419146586128413,63.853164036092657]]]}},{"type":"Feature","properties":{"id":"1"},"geometry":{"type":"MultiLineString","coordinates":[[[-22.413916823241227,63.856984837982068],[-22.415605972371829,63.856075170349094]]]}},{"type":"Feature","properties":{"id":"1"},"geometry":{"type":"MultiLineString","coordinates":[[[-22.412472528645775,63.858035930605915],[-22.414143457495037,63.857136109040376]]]}}]}
+var json_Fissure20250401_18 = {
+"type": "FeatureCollection",
+"name": "Fissure20250401_18",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": 1 }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ -22.407867973240396, 63.861355287866814 ], [ -22.412564592978629, 63.858141507255027 ] ] ] } },
+{ "type": "Feature", "properties": { "id": 2 }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ -22.418651691357582, 63.854396756926292 ], [ -22.418944974611151, 63.853878528178065 ] ] ] } },
+{ "type": "Feature", "properties": { "id": 1 }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ -22.414346636887512, 63.856870618902981 ], [ -22.415671782633517, 63.856080722407341 ] ] ] } },
+{ "type": "Feature", "properties": { "id": 1 }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ -22.412472528645775, 63.858035930605915 ], [ -22.414143457495037, 63.857136109040376 ] ] ] } },
+{ "type": "Feature", "properties": { "id": 2 }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ -22.418895989985568, 63.853704595834785 ], [ -22.419236520674609, 63.853032147654609 ] ] ] } }
+]
+}
