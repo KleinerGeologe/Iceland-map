a7f517444c948cb2c3f8192da7d886127ff33dda
KleinerGeologe <david.guenzler@yahoo.de>
2025-07-16 12:02:47 +0200

    new fissure added

---
+++ data/Fissure20250716_19.js
@@ -0,0 +1,12 @@
+var json_Fissure20250716_19 = {
+"type": "FeatureCollection",
+"name": "Fissure20250716_19",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": 1 }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ -22.347179371678845, 63.90529601285418 ], [ -22.352779129981634, 63.901461513416976 ] ] ] } },
+{ "type": "Feature", "properties": { "id": null }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ -22.340299894632057, 63.910125001867726 ], [ -22.33884503936088, 63.910737504833726 ] ] ] } },
+{ "type": "Feature", "properties": { "id": null }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ -22.343061077910104, 63.908383967297766 ], [ -22.340463237459485, 63.909660589131036 ] ] ] } },
+{ "type": "Feature", "properties": { "id": null }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ -22.344739478280353, 63.907279213610032 ], [ -22.342625845996242, 63.908333543374226 ] ] ] } },
+{ "type": "Feature", "properties": { "id": null }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ -22.344729055118819, 63.907061474205946 ], [ -22.346822940228993, 63.905598852852691 ] ] ] } }
+]
+}
